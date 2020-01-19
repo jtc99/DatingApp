07ceaed37415307746f4a2494a0c3af075f8e76a
J. Crowley <jtcrowley3@gmail.com>
2020-01-19 15:42:21 +0000

    Lesson 30 Created Services

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -27,10 +27,10 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite
-            (Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
